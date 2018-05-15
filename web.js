963303818780ba6e41fe3d5160a040ff8ce74049
Anton-E-Guru <antoneguru@gmail.com>
2018-05-15 16:32:09 +0300

    Add fetchObject, request console logging

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var http = require('http');
-
-function onRequest(request,response) {
-	console.log("Request received:");
+function fetchObject(obj) {
+	for (var key in obj) {
+		var val=obj[key];
+		if (val != null)
+			if (typeof(val)=='string')
+				return val;
+			else if (typeof(val) == 'object')
+				return fetchObject(val);
+			else 
+				return null;
+	}
+}
+http.createServer(function(request,response) {
+	console.log("Request received");
+	for (var key in request) {
+		var  reqVal=request[key];
+			console.log("Key:"+key+"Value:"+fetchObject(reqVal));
+	}
 	response.writeHead(200,{"Content-Type":"text/plain"});
 	response.write("hello world, this is my first NodeJS APP");
 	response.end();
 	
-}.listen(8888);
+}).listen(8888);
